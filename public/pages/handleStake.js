17e20805882343d06a70439022befcb3014c13b1
unknown <ugoi@protonmail.ch>
2025-03-05 21:50:53 +0300

    Created a process to handle staking of tokens

---
+++ public/pages/handleStake.js
@@ -0,0 +1,41 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    let stakeInput = document.getElementById('tokenInput');
+    let submitButton = document.getElementById('submitStakeButton');
+    let cancelButton = document.getElementById('cancelStakeButton');
+
+    // Capture null errors
+    if (!stakeInput || !submitButton || !cancelButton) {
+        console.error("one or more elements not found!");
+        return;
+    }
+
+    // Function to capture stake input and send to API
+    function tokenStake ( tokens ) {
+        console.log('First point of log',tokens)
+        
+        // Fetch data from HTML and send to transact API 
+        fetch('http://localhost:3000/api/staketoken', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json' },
+            body: JSON.stringify({ tokens })
+        })
+    }
+
+
+    // Event listener for the SUBMIT button on HTML page
+    submitButton.addEventListener('click', function () {
+        tokenStake( stakeInput.value );
+        console.log('Second point of log', stakeInput.value );
+
+    });
+
+
+    // Event listener for the CANCEL button on HTML page
+    cancelButton.addEventListener('click', function () {
+        console.log('Transaction Canceled');
+        alert('File uploaded successfully!');
+        })
+
+
+})
