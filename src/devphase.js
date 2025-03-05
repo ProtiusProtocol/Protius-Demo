4610d6f5cbdb4d71681ff52aad697fc73a9c8845
unknown <ugoi@protonmail.ch>
2025-03-05 21:49:54 +0300

    Created a javascript file with a developmentphase class object to house the functions for the development phase

---
+++ src/devphase.js
@@ -0,0 +1,95 @@
+
+
+class ProjectDevelopmentPhase {
+    constructor ( input ){
+        this.input = this.input
+    }
+    
+    verifyLandTenure(){
+
+    }
+
+    verifyServitueLeeway (){
+
+    }
+
+    verifyResourceStudy (){
+
+    }
+
+    verifyGridConnection () {
+
+    }
+
+    verifyGridConnectionAgreement () {
+
+    }
+
+    verifyEnvironmentalReview (){
+
+    }
+
+    verifyPublicParticipation () {
+
+    }
+
+    verifyPermits () {
+
+    }
+
+    verifyPowerPurchaseAgreemnet (){
+
+    }
+
+    verifyPlantDesign(){
+
+    }
+
+    verifyEPC () {
+
+    }
+
+    verifyDebtProvider() {
+
+    }
+
+    verifyEquityProvider() {
+
+    }
+
+    verifyFinancialModel (){
+
+    }
+
+    verifyFinancialModelAudit (){
+
+    }
+
+    verifyEquipmentSupplier(){
+
+    }
+
+    verifyRoadsLogisticsPlan(){
+
+    }
+
+    verifyTaxPlan(){
+
+    }
+
+    verifyStakeholders() {
+
+    }
+
+    verifyCommunityEngagementPlan(){
+
+    }
+
+    verifyAccDevelopmentSpendOnBudget(){
+
+    }
+
+    completeDevelopmentPhase(){
+        
+    }
+}
