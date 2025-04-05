4336aa8b228298ca02478de636e20c380fdc44f1
unknown <ugoi@protonmail.ch>
2025-04-05 07:47:51 +0300

    Updated function to create new project onchain

--- src/devphase.js
+++ src/devphase.js
@@ -1,11 +1,47 @@
+const { generateUUID } = require("pubnub")
 
 
 class ProjectDevelopmentPhase {
-    constructor ({ }){
-        this.input = input
-        this.projectID = projectID
+    constructor ( projectName, projectDeveloper, projectPhases ){
+        this.projectName = projectName,
+        this.projectDeveloper = projectDeveloper,
+        this.projectPhases = projectPhases,
+        this.projectID = `ID-${crypto.randomUUID()}`
     }
     
+
+    createNewProject( projectName, projectDeveloper){
+        let projectID, projectStatus, projectPhases;
+
+        projectID = `ID-${crypto.randomUUID()}`;
+        projectStatus = "Ongoing";
+        projectPhases = {
+            landTenure: "Pending",
+            servitueLeeway: "Pending",
+            resourceStudy: "Pending",
+            gridConnection: "Pending",
+            gridConnectionAgreement: "Pending",
+            environmentalReview: "Pending",
+            publicParticipation: "Pending",
+            permits: "Pending",
+            PowerPurchaseAgreemnet: "Pending",
+            PlantDesign: "Pending",
+            EPC: "Pending",
+            DebtProvider: "Pending",
+            EquityProvider: "Pending",
+            financialModel: "Pending",
+            financialModelAudit: "Pending",
+            equipmentSupplier: "Pending",
+            roadsLogisticsPlan: "Pending",
+            taxPlan: "Pending",
+            stakeholders: "Pending",
+            communityEngagementPlan: "Pending",
+            accDevelopmentSpendOnBudget: "Pending",
+            completeDevelopmentPhase: "Pending"
+        }
+        return { projectName, projectDeveloper, projectID, projectStatus, projectPhases}
+    }
+
     verifyLandTenure(){
 
     }
@@ -94,3 +130,5 @@ class ProjectDevelopmentPhase {
 
     }
 }
+
+module.exports = ProjectDevelopmentPhase;
