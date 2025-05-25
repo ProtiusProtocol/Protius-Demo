a9d6a7045bf8fcdf06596b41b1b6005c705cd11f
ugo@ahtiso.com <ugo@ahtiso.com>
2025-05-25 17:20:25 +0300

    Updated code to point to local vote API

--- public/pages/voteHandler.js
+++ public/pages/voteHandler.js
@@ -8,12 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
     let noButton = document.getElementById('noButton');
     let undecidedButton = document.getElementById('undecidedButton');
 
-    function voteProcess( /*proposalNumber,*/ userVote ){
+    function voteProcess( userVote ){
         console.log('User voted:', userVote, 'to Proposal');
 
         // For all Dapps, this fetch should connect to blockchain backend
-        fetch('https://ahtiso-dios-test-network-02d729532570.herokuapp.com/api/vote', {
-        //fetch('http://localhost:3000/api/vote', {
+        //fetch('https://ahtiso-dios-test-network-02d729532570.herokuapp.com/api/vote', {
+        fetch('http://localhost:4000/api/vote', {
                 method: 'POST',
             headers: {'Content-Type': 'application/json' },
             body: JSON.stringify({ userVote })
