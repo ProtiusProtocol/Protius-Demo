732089a5c017ac45ebc438bf1939ad9811a2d726
ugo@ahtiso.com <ugo@ahtiso.com>
2025-06-30 21:56:48 +0300

    Function to update database

---
+++ src/updateDatabase.js
@@ -0,0 +1,55 @@
+
+const mysql = require('mysql2');
+
+async function insertProjectAndPhases(newProject) {
+  const conn = await mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'project@2025',
+    database: 'protius'
+  }).promise();
+  console.log("Connected to database");
+
+  try {
+    await conn.beginTransaction();
+
+    // Insert project
+    await conn.execute(
+      `INSERT INTO projects (projectID, projOwner, projectName, projectDeveloper, projectStatus, projectFinance, developerContribution)
+       VALUES (?, ?, ?, ?, ?, ?, ?)`,
+      [
+        newProject.projectID,
+        newProject.projOwner,
+        newProject.projectName,
+        newProject.projectDeveloper,
+        newProject.projectStatus,
+        newProject.projectFinance,
+        newProject.developerContribution
+      ]
+    );
+
+    // Insert phases
+    const phaseEntries = Object.entries(newProject.projectPhases);
+    const phaseInserts = phaseEntries.map(([phaseName, phaseStatus]) =>
+      conn.execute(
+        `INSERT INTO project_phases (projectID, phaseName, phaseStatus)
+         VALUES (?, ?, ?)`,
+        [newProject.projectID, phaseName, phaseStatus]
+      )
+    );
+
+    await Promise.all(phaseInserts);
+
+    await conn.commit();
+    console.log('Insert successful');
+  } catch (err) {
+    await conn.rollback();
+    console.error('Insert failed:', err);
+    throw err;
+  } finally {
+    await conn.end();
+  }
+}
+
+module.exports = { insertProjectAndPhases };
