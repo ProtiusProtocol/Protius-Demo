4d54f08eced23d7f143628e61df0ffc296c21d21
ugo@ahtiso.com <ugo@ahtiso.com>
2025-06-30 21:17:31 +0300

    Added functionality to update the event list whenever new projects are created

--- public/pages/create.js
+++ public/pages/create.js
@@ -2,31 +2,61 @@
 document.addEventListener('DOMContentLoaded', () =>{
     let projectNameHTML = document.getElementById('projName');
     let projectDeveloperHTML = document.getElementById('projDev');
+    let projectFinanceHTML = document.getElementById('projFin');
+    let devContributionHTML = document.getElementById('devFinanceInput');
     let createButton = document.getElementById('createProjectButton');
     let cancelButton = document.getElementById('cancelButton');
 
     // Capture null errors
-    if (!projectNameHTML || !projectDeveloperHTML || !createButton || !cancelButton) {
+    if (!projectNameHTML || !projectDeveloperHTML || !projectFinanceHTML || !devContributionHTML || !createButton || !cancelButton) {
         console.error("one or more elements not found!");
         return;
     }
 
     // Function to send user towards project creation
-    function handleProjectCreation( newName, newDeveloper ){
+    function handleProjectCreation( owner, newName, newDeveloper, finance, devContribute ){
 
         fetch('http://localhost:4000/api/createproject', {
             method: 'POST',
             headers: {'content-type': 'application/json'},
-            body: JSON.stringify({ newName, newDeveloper })
+            body: JSON.stringify({ owner, newName, newDeveloper, finance, devContribute })
             })
-        }
-    
+
+        fetch("http://localhost:4000/api/newevent", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                type: "Frontend Event,",
+                event: "Project Created,",
+                user: owner,
+                payload: { newName, newDeveloper, finance },
+                timestamp: new Date().toISOString().slice(0, 19).replace('T', ' ')
+                })
+            });
+    };
+        
+
     // Event listener for the SUBMIT button on HTML page
     createButton.addEventListener('click', function () {
-        handleProjectCreation( projectNameHTML.value, projectDeveloperHTML.value );
+        const projectOwner = localStorage.getItem('connectedWallet');
+
+        if ( !projectOwner ) {
+            console.error("Project Owner not found, connect a wallet!");
+            return;
+        }
+
+        handleProjectCreation( 
+            projectOwner, 
+            projectNameHTML.value, 
+            projectDeveloperHTML.value,
+            projectFinanceHTML.value, 
+            devContributionHTML.value
+        );
         //location.reload();
+
     });
 
+    
     // Event listener for the CANCEL button on HTML page
     cancelButton.addEventListener('click', function () {
         console.log('Process cancelled!');
