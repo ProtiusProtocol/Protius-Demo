7cc9a3b316150eefad3a9af562275219bd6dce64
ugo@ahtiso.com <ugo@ahtiso.com>
2025-10-17 09:23:15 +0300

    Process to check total USDC supply

---
+++ public/pages/check-usdc-supply.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const networkDiv = document.getElementById('connected-network');
+    const checkBalance = document.getElementById('request-network-info');
+
+    async function checkUSDCSupply(){
+      const res = await fetch('http://localhost:4000/api/check-usdc-supply');
+      //const res = await fetch('https://protius-demo-v1-3ec4758d01ce.herokuapp.com/api/check-usdc-supply', {
+
+      const data = await res.json();
+      console.log(data)
+      return data.networkCon;
+    }
+
+    checkBalance.addEventListener('click', async () => {
+      
+      const info = await checkUSDCSupply();
+      //networkDiv.textContent = `Network: ${info}`;
+      console.log('USDC Supply:', info)
+
+    })
+
+    
+});
